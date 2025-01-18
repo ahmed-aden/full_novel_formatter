b149c452ca33d54e99c4c5fd00af3ff8877cad66
Ahmed <ahmed.aden14@icloud.com>
2025-01-18 23:03:50 +1100

    create scraper for main page

--- src/get_links.py
+++ src/get_links.py
@@ -1,3 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+URL = "https://novelfull.com/lord-of-the-mysteries.html"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+container = soup.find(id="list-chapter")
+base_url = "https://novelfull.com/"
+links = container.find_all("a")
+
+# links for everything on page 1
+href_values = [urljoin(base_url, link.get('href')) for link in links]
+
+
+
+
+
+
+
+
+
+
+
 
