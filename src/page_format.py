d245bf82371478af4f4a01928eec1681a06afea7
Ahmed <ahmed.aden14@icloud.com>
2025-01-19 22:28:17 +1100

    modularised file

--- src/page_format.py
+++ src/page_format.py
@@ -2,15 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://novelfull.com/lord-of-the-mysteries/chapter-1-crimson.html"
-page = requests.get(URL)
+
+def get_story(URL: str):
+    # get the page from the URL
+    page = requests.get(URL)
+    # scrape the html off the page
+    soup = BeautifulSoup(page.content, "html.parser")
+    # get the chapter content
+    story = soup.find(id="chapter-content")
+    # get the story in the chapter
+    content = story.find_all("p")
+    # for each line in the story
+    for line in content:
+        # strip the useless stuff, and add 2 newlines
+        print(line.text.strip(), end="\n" * 2)
 
 
-soup = BeautifulSoup(page.content, "html.parser")
+def main():
+    # TODO replace URL with it being passed from another function which scrapes URLS from the main page
+    URL = "https://novelfull.com/lord-of-the-mysteries/chapter-1-crimson.html"
+    get_story(URL)
 
-story = soup.find(id="chapter-content")
-content = story.find_all("p")
-
-for line in content:
-    print(line.text.strip(), end="\n" *2)
+if __name__ == '__main__':
+    main()
     
+
