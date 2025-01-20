72a873f2cfc6b40f556d15a82afae69ddf550a65
Ahmed <ahmed.aden14@icloud.com>
2025-01-20 17:02:23 +1100

    needs fix

--- src/page_format.py
+++ src/page_format.py
@@ -10,22 +10,35 @@ def get_story(URL: str):
     soup = BeautifulSoup(page.content, "html.parser")
     # get the title tag
     
+    # print(soup.prettify()[:5000])  
     
     # get the chapter content
-    story = soup.find(id="chapter-content")
+    # story = soup.find(id="chapter-content")
+    chapter_content = soup.find('div', {'id': 'chapter-content'})
+    # print(story.prettify()[:5000])  
     # get the story in the chapter
-    content = story.find_all("p")
+    if not chapter_content:
+        print("chp content not found")
+        return
     
     # alr i got the chapter title
-    chapter_title = content[2].text.strip()
-    print(f" chapter title: {chapter_title}")
+    chapter_title_tag = chapter_content.find('h3')
+    if chapter_title_tag:
+        chapter_title = chapter_title_tag.text.strip()
+    else:
+        chapter_title = "Unknown Title"
+    
+    print(f"Chapter title: {chapter_title}")
+
+
     chapter_text = ""
-    for line in content:
+    for line in chapter_content.find_all('p'):
         # strip the useless stuff, and add 2 newlines
         chapter_text += line.text.strip() + "\n\n" 
     
+    
     # save to txt file
-    chapter_number = 1 # change chapter number to be dynamic
+    chapter_number = 2 # change chapter number to be dynamic
     # create directory
     directory = "chapters"
     # create dir if it does not exist
@@ -39,7 +52,8 @@ def get_story(URL: str):
 def main():
     # TODO replace URL with it being passed from another function which scrapes URLS from the main page
     URL = "https://novelfull.com/lord-of-the-mysteries/chapter-1-crimson.html"
-    get_story(URL)
+    URL2 = "https://novelfull.com/lord-of-the-mysteries/chapter-2-situation.html"
+    get_story(URL2)
     # what page format does
     # is given a chapter link
     # it will save it to a txt file
@@ -50,5 +64,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
     
 
