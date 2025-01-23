a2b9eaf1027231f85bddacc02dc3b9f5436d5e66
Ahmed <ahmed.aden14@icloud.com>
2025-01-24 00:22:47 +1100

    create epub conversion

--- src/page_format.py
+++ src/page_format.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-# from get_links import scrape
+import subprocess
 
 def scrape(URL: str) -> str:
     """:returns: html content from page"""
@@ -9,13 +9,6 @@ def scrape(URL: str) -> str:
     soup = BeautifulSoup(page.content, "html.parser")
     return soup
 
-
-
-# REFACTORING
-# TODO: create function to get the titles
-# TODO: get a function to create a new txt page
-# TODO: 
-# TODO
 def get_title(header: str) -> str:
     chapter_title_tag = header.find('h3')
     if chapter_title_tag:
@@ -24,48 +17,61 @@ def get_title(header: str) -> str:
         chapter_title = "Unknown Title"
     return chapter_title
 
+def convert_txt_to_epub(txt_file: str, epub_file: str) -> None:
+    """converts txt files in chapters to epub""" 
+    # i think we can call this function in write to chapters
+    cmd = ["pandoc", txt_file, "-o", epub_file]
+    subprocess.run(cmd, check=True)
+    print("Conversion successful")
 
-def get_story(URL: str) -> None:
+def write_to_chapters_dir(page: str) -> None:
+    """creates a txt file within the chapters directory and writes to the txt file."""
+    directory = "chapters"
+    directory_epub = "chapters-epub"
+    chapter_title = page.partition('\n')[0] # make this receive an integer as a function
     
+    # there is no directory make as this or file..
+
+    # this checks to see if there is a directory, and if not, creates one.
+    os.makedirs(directory, exist_ok=True)
+    
+    with open(f"{directory}/{chapter_title}.txt", 'w') as writer:
+        writer.write(page)
+    os.makedirs(directory_epub, exist_ok=True)
+    # convert_txt_to_epub(f"{directory}/{chapter_title}.txt", f"{directory_epub}/{chapter_title}.epub")
+
+
+def get_story(URL: str) -> str:
     soup = scrape(URL)
     chapter_content = soup.find('div', {'id': 'chapter-content'})
     # this is the content within the page
     next_div = chapter_content.find_next_sibling('div') 
     chapter_title = get_title(chapter_content)
     print(f"Chapter title: {chapter_title}")
-
-
     chapter_text = f"{chapter_title} \n\n" 
     # strip away the html from the content
     for line in next_div.find_all('p'):
         chapter_text += line.text.strip() + "\n\n" 
-    
-    # save to txt file
-    chapter_number = 2 # change chapter number to be dynamic
-    # create directory
-    directory = "chapters"
-    # create dir if it does not exist
-    os.makedirs(directory, exist_ok=True)
-    with open(f'{directory}/chapter{chapter_number}.txt', 'w') as f:
-        f.write(chapter_text)
-    
-    
+
+    return chapter_text
+
+
+def join_story() -> None:
+    """"""
+    # write to a txt file
+    # write_to_chapters_dir(chapter_text)
+
+
+
 
 
 def main():
-    # TODO replace URL with it being passed from another function which scrapes URLS from the main page
+    # URLS for testing
     URL = "https://novelfull.com/lord-of-the-mysteries/chapter-1-crimson.html"
     URL4 = "https://novelfull.com/lord-of-the-mysteries/chapter-4-divination.html"
-    get_story(URL4)
+    URL2 = "https://novelfull.com/lord-of-the-mysteries/chapter-2-situation.html"
+    get_story(URL2)
     
-    # what page format does
-    # is given a chapter link
-    # it will save it to a txt file
-    # now i want to be able to put all these txt files in a directory
-        # create directory if not existing
-        # save all txt file to directory
-        
-
 if __name__ == '__main__':
     main()
     
